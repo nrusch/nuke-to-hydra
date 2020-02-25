4a921972948218cd6ab59498ddb92217918fb1af
nrusch <nrusch@github>
2020-02-24 23:15:44 -0800

    [utils] Convert file knobs to SdfAssetPath, enumeration knobs to TfToken

--- src/hdNuke/utils.cpp
+++ src/hdNuke/utils.cpp
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+#include <pxr/usd/sdf/assetPath.h>
+
+#include <DDImage/Enumeration_KnobI.h>
 #include <DDImage/Knobs.h>
 
 #include "utils.h"
@@ -35,8 +38,13 @@ KnobToVtValue(const Knob* knob)
         case BOOL_KNOB:
             return VtValue(static_cast<bool>(knob->get_value()));
         case INT_KNOB:
-        case ENUMERATION_KNOB:
             return VtValue(static_cast<int>(knob->get_value()));
+        case ENUMERATION_KNOB:
+        {
+            const Enumeration_KnobI* enumKnob =
+                const_cast<Knob*>(knob)->enumerationKnob();
+            return VtValue(TfToken(enumKnob->getSelectedItemString()));
+        }
         case COLOR_KNOB:
         case XYZ_KNOB:
             return VtValue(GfVec3f(knob->get_value(0), knob->get_value(1),
@@ -45,10 +53,15 @@ KnobToVtValue(const Knob* knob)
             return VtValue(GfVec4f(knob->get_value(0), knob->get_value(1),
                                    knob->get_value(2), knob->get_value(3)));
         case STRING_KNOB:
-        case FILE_KNOB:
             {
                 const char* rawString = knob->get_text();
                 return VtValue(std::string(rawString ? rawString : ""));
+            }
+        case FILE_KNOB:
+            {
+                const char* rawString = knob->get_text();
+                const std::string path(rawString ? rawString : "");
+                return VtValue(SdfAssetPath(path, path));
             }
         default:
             TF_WARN("KnobToVtValue : No VtValue conversion implemented for "
